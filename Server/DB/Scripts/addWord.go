d80f80aed5a8c7324ba7702eec5fc161a7826e86
dstr951 <dstr159@gmail.com>
2021-05-06 23:07:18 +0300

    new add word to db function

---
+++ Server/DB/Scripts/addWord.go
@@ -0,0 +1,19 @@
+package Scripts
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"ishtaloo.io/DB/Collections"
+	entities "ishtaloo.io/ENTITIES"
+)
+
+func AddWord(ctx context.Context, word entities.Word) {
+	wordsCollection := Collections.WordsGetCollection()
+	newWordsResult, err := wordsCollection.InsertOne(ctx, word)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted single document: ", newWordsResult.InsertedID)
+}
