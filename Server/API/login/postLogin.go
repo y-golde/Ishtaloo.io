3460f2f4f18a98368d3bdd7c2013283734180ffa
dstr951 <dstr159@gmail.com>
2021-05-07 11:47:14 +0300

    send basic cookie with succesfull login post request

--- Server/API/login/postLogin.go
+++ Server/API/login/postLogin.go
@@ -1,9 +1,21 @@
 package login
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
 
 func postLogin(c echo.Context) (err error) {
+	cookie := new(http.Cookie)
+	cookie.Name = "userId"
+	cookie.Value = createToken()
+	cookie.HttpOnly = true
+	c.SetCookie(cookie)
+	return c.String(http.StatusOK, "login successful")
+}
 
+func createToken() string {
+	s := "hello"
+	return s
 }
