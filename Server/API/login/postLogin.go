75c84312390e89e6d7e84c60473a33a0b3ea80ca
dstr951 <dstr159@gmail.com>
2021-05-07 12:51:58 +0300

    added a uuid userId to each jwt token

--- Server/API/login/postLogin.go
+++ Server/API/login/postLogin.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	entities "ishtaloo.io/Entities"
 )
@@ -17,8 +18,10 @@ func postLogin(c echo.Context) (err error) {
 	if err = c.Bind(u); err != nil {
 		return err
 	}
+	id := uuid.New()
 	loginParams := entities.LoginRequest{
 		UserName: u.UserName,
+		UserId:   id.String(),
 	}
 	cookie.Value, err = createToken(loginParams)
 	if err != nil {
@@ -34,6 +37,7 @@ func createToken(loginParams entities.LoginRequest) (string, error) {
 	atClaims := jwt.MapClaims{}
 	atClaims["authorized"] = true
 	atClaims["user_name"] = loginParams.UserName
+	atClaims["user_id"] = loginParams.UserId
 	atClaims["exp"] = time.Now().Add(time.Hour * 24).Unix()
 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
 	token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
--- Server/Entities/LoginRequest.go
+++ Server/Entities/LoginRequest.go
@@ -2,4 +2,5 @@ package entities
 
 type LoginRequest struct {
 	UserName string `json:"userName" bson:"userName"`
+	UserId   string `json:"userId" bson:"userId"`
 }
