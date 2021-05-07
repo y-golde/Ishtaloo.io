6b7e8bf8f4d5a8958212a1a5c03ff05517a1735b
y-golde <yon.golde@gmail.com>
2021-05-07 08:49:52 +0300

    font

--- Client/src/App.svelte
+++ Client/src/App.svelte
@@ -10,13 +10,13 @@ import ThemeWrapper from './CSS/ThemeWrapper.svelte';
   @tailwind base;
   @tailwind components;
   @tailwind utilities;
-  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400&display=swap');
   html {
 	  background-color: var(--background);
   }
 
   html * {
 	font-family: 'Source Sans Pro', sans-serif;
+	font-weight: 400;
   }
 </style>
 
