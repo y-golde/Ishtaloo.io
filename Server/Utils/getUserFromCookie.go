a2ccb11cad5cec55e0d41221a89188c0377a194f
dstr951 <dstr159@gmail.com>
2021-05-08 22:23:31 +0300

    function to return User from cookie

---
+++ Server/Utils/getUserFromCookie.go
@@ -0,0 +1,24 @@
+package Util
+
+import (
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+	entities "ishtaloo.io/Entities"
+)
+
+func GetUserFromCookie(c echo.Context) (*entities.User, error) {
+	//user := new(entities.User)
+	cookie, err := c.Cookie("user")
+	if err != nil {
+		return nil, err
+	}
+	u, _ := decodedToken(cookie.Value)
+	fmt.Println(u)
+
+	user := &entities.User{
+		UserName: u["user_name"].(string),
+		UserId:   u["user_id"].(string),
+	}
+	return user, nil
+}
