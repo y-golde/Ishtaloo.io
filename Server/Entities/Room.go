dd600a596ae5c41021f8ff708a143818fb44a291
dstr951 <dstr159@gmail.com>
2021-05-08 22:23:49 +0300

    Room struct

--- Server/Entities/Room.go
+++ Server/Entities/Room.go
@@ -1,8 +1,8 @@
 package entities
 
 type Room struct {
-	RoomId       string `json:"roomId" bson:"room_id"`
-	CurrentWord  []rune `json:"currentWord" bson:"current_word"`
-	WrongGuesses []rune `json:"wrongGuesses" bson:"wrong_guesses"`
-	Users        []User `json:"users" bson:"users"`
+	RoomId      string   `json:"roomId" bson:"room_id"`
+	CurrentWord []string `json:"currentWord" bson:"current_word"`
+	Guesses     []string `json:"Guesses" bson:"guesses"`
+	Users       []User   `json:"users" bson:"users"`
 }
