1e81067e2d24473879d5871a769d79f6ccc4adff
dstr951 <dstr159@gmail.com>
2021-05-15 19:26:13 +0300

    small fxies

--- Server/Entities/Room.go
+++ Server/Entities/Room.go
@@ -3,6 +3,6 @@ package entities
 type Room struct {
 	RoomId      string   `json:"roomId" bson:"_id,omitempty"`
 	CurrentWord []string `json:"currentWord" bson:"current_word"`
-	Guesses     []string `json:"Guesses" bson:"guesses"`
+	Guesses     []string `json:"guesses" bson:"guesses"`
 	Users       []User   `json:"users" bson:"users"`
 }
