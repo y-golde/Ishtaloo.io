db3f13b22a4d56fd95f7dc49d46bb42118aa4b2a
dstr951 <dstr159@gmail.com>
2021-05-16 12:58:01 +0300

    use GetWordsByFilter in GET request at /words

--- Server/API/words/getWords.go
+++ Server/API/words/getWords.go
@@ -1,30 +1,14 @@
 package words
 
 import (
-	"context"
-	"fmt"
-	"log"
 	"net/http"
-	"time"
 
 	"github.com/labstack/echo/v4"
 	"go.mongodb.org/mongo-driver/bson"
-	"ishtaloo.io/DB/Collections"
+	wordsScripts "ishtaloo.io/DB/Scripts/words"
 )
 
 func getWords(c echo.Context) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	wordsCollection := Collections.WordsGetCollection()
-	cursor, err := wordsCollection.Find(ctx, bson.M{})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var words []bson.M
-	if err = cursor.All(ctx, &words); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(words)
+	words := wordsScripts.GetWordsByFilter(bson.M{})
 	return c.JSON(http.StatusOK, words)
 }
