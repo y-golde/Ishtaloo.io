5ddee77648b848df8c4c0ff8ab49821b55d870de
y-golde <yon.golde@gmail.com>
2021-05-17 15:47:08 +0300

    added roomid to portal

--- Client/src/Components/App/Game/Game.svelte
+++ Client/src/Components/App/Game/Game.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import Users from './Users/Users.svelte';
 	import GamePanel from './GamePanel/GamePanel.svelte';
+
+	export let roomId: string;
+
+	console.log('elo', roomId, 'aaaa');
 </script>
 
 <div class="container mx-auto">
