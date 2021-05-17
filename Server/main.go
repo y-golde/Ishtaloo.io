6c130d4c14a922227eb1b59a9e737a8a1a7eaeea
dstr951 <dstr159@gmail.com>
2021-05-17 19:05:05 +0300

    basic example for sse

--- Server/main.go
+++ Server/main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
-	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"go.mongodb.org/mongo-driver/bson"
 	rootController "ishtaloo.io/API"
 	"ishtaloo.io/DB"
-	"ishtaloo.io/DB/Collections"
+	entities "ishtaloo.io/Entities"
 )
+
+var sseChannel entities.SSEChannel
 
 func main() {
 	DB.InitClient()
@@ -33,27 +34,89 @@ func main() {
 
 	// change with dotenv later to remove origins
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-  		AllowOrigins: []string{"http://localhost:5000"},
+		AllowOrigins:     []string{"http://localhost:5000"},
 		AllowCredentials: true,
 	}))
 
 	// Route => handler
 	rootController.RootController(e)
 
-	e.GET("/api", func(c echo.Context) error {
-		fmt.Printf("/API/ WAS CALLED")
-		usersCollection := Collections.UsersGetCollection()
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
+	// initialize global variable
+	sseChannel = entities.SSEChannel{
+		Clients:  make([]chan string, 0),
+		Notifier: make(chan string),
+	}
 
-		cur, _ := usersCollection.Find(ctx, bson.M{})
+	// done signal to go routine.
+	done := make(chan interface{})
+	defer close(done)
 
-		var users []bson.M
-		err = cur.All(ctx, &users)
+	// run our broadcaster go routine.
+	go broadcaster(done)
 
-		return c.JSON(200, users)
-	})
+	e.GET("/sse", sseRequest)
+	e.GET("/log", logRequest)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
+}
+
+func broadcaster(done <-chan interface{}) {
+	fmt.Println("Broadcaster Started.")
+	for {
+		select {
+		case <-done:
+			return
+		case data := <-sseChannel.Notifier:
+			for _, channel := range sseChannel.Clients {
+				channel <- data
+			}
+		}
+	}
+}
+
+func logRequest(c echo.Context) error {
+	//buf := new(strings.Builder)
+	var body string
+	if err := c.Bind(&body); err != nil {
+		return c.String(http.StatusForbidden, "body bad format.")
+	}
+
+	logMsg := fmt.Sprintf("Method: %v, Body: %v", "GET", body)
+	fmt.Println(logMsg)
+	sseChannel.Notifier <- logMsg
+	return nil
+}
+
+func sseRequest(c echo.Context) error {
+
+	c.Response().Header().Set(echo.HeaderContentType, "text/event-stream")
+	c.Response().Header().Set("Cache-Control", "no-cache")
+	c.Response().Header().Set("Connection", "keep-alive")
+	//c.Response().Header().Set("Access-Control-Allow-Origin", "*")
+	c.Response().WriteHeader(http.StatusOK)
+
+	enc := json.NewEncoder(c.Response())
+	if err := enc.Encode("new user join"); err != nil {
+		return err
+	}
+
+	sseChan := make(chan string)
+	sseChannel.Clients = append(sseChannel.Clients, sseChan)
+
+	d := make(chan interface{})
+	defer close(d)
+	defer fmt.Println("Closing channel.")
+
+	for {
+		select {
+		case <-d:
+			close(sseChan)
+			return c.NoContent(http.StatusOK)
+		case data := <-sseChan:
+			fmt.Printf("data: %v \n\n", data)
+			fmt.Fprintf(c.Response(), "data: %v \n\n", data)
+			c.Response().Flush()
+		}
+	}
 }
